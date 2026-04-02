575830a3dc45d4a427cb6b08e66bb0d7fec8736b
Yuanbeiming <2890535673@qq.com>
2026-04-03 02:16:54 +0800

    Update DIO_WORLD.py

--- DIO_WORLD.py
+++ DIO_WORLD.py
@@ -829,8 +829,7 @@ def reasoning(*args):
 
  
 if __name__ == '__main__':
-    #from torchsummary import summary
-    from torchinfo import summary
+    
     num_item = 1
     x = torch.randn(num_item,16,80,80)
     y = torch.randint(1,(num_item,7776,16)).long()
@@ -840,20 +839,6 @@ if __name__ == '__main__':
     model = raven_clip()
     
     model.eval()
-    # params = torch.load('./model_Clip_raven_120000_distribute_nine_best.pt', map_location = 'cpu')
-    # # model_dict =  model.state_dict()
-    
-    # # state_dict = {k:v for k,v in params.items() if k in model_dict.keys()}
-    # for k,q in model.named_parameters():
-    #     if k[:7] != 'tajador':
-    #         print(k)
-    #         q.data = params[k].data
-            
-
-    # 
-    # model_dict.update(state_dict)
-    # model.load_state_dict(model_dict)       
-    # model.load_state_dict(torch.load('./model_Clip_raven_120000_distribute_nine_best.pt', map_location = 'cpu'))
     out = model(x)
     
     l, right_shape, right_line, right = model.loss_function(*out, target_shape = target, target_line = target, idx = label)
@@ -861,12 +846,12 @@ if __name__ == '__main__':
 
     print(model)
 
-    #model = raven_clip().to('cuda' if torch.cuda.is_available() else 'cpu')
-    #summary(model, (16, 80, 80), device='cpu')
+    from torchinfo import summary
     summary(model, input_size=(1, 16, 80, 80),
         col_names=["input_size", "output_size", "num_params", 
                     "kernel_size", "mult_adds", "trainable"], device='cpu')
     #accuracy = model.choose_accuracy(*out, idx = label)
+    
     from torch.utils.benchmark import Timer
 
     stmt = "model(input_tensor)"
@@ -886,6 +871,7 @@ if __name__ == '__main__':
 
     result = timer.blocked_autorange(min_run_time=10)  # 至少跑10秒
     print(f"{result.median*1000:.3f} ms ± {result.iqr*1000:.3f} ms")
+
 
     
     
